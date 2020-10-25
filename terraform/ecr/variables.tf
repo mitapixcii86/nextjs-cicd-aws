399a7763b974426d5f8e899fecdb9effd19b46dd
Moumita Bakshi <moumitabakshi@Moumitas-MacBook-Pro.local>
2020-10-25 16:37:25 +0100

    ecr infra privisioning and pushing docker image to ecr

---
+++ terraform/ecr/variables.tf
@@ -0,0 +1,30 @@
+variable "image_name" {
+  description = "Name of Docker image"
+  type        = string
+  default = "nextjs-app"
+}
+
+variable "source_path" {
+  description = "Path to Docker image source"
+  type        = string
+  default = "../../app/"
+}
+
+variable "tag" {
+  description = "Tag to use for deployed Docker image"
+  type        = string
+  default     = "latest"
+}
+
+variable "hash_script" {
+  description = "Path to script to generate hash of source contents"
+  type        = string
+  default     = ""
+}
+
+variable "push_script" {
+  description = "Path to script to build and push Docker image"
+  type        = string
+  default     = ""
+}
+
--- terraform/main.tf
+++
@@ -1,8 +0,0 @@
-provider "aws" {
-  version = "~> 2.0"
-  region  = "eu-west-2" # Setting my region to London. Use your own region here
-}
-
-resource "aws_ecr_repository" "devops_ecr" {
-  name = "devops-ecr" # Naming my repository
-}
